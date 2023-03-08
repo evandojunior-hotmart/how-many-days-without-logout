2967a271ea379f8370dcb982a2527e5fc46fa999
Evando Junior <evando.junior@hotmart.com>
2023-03-08 01:23:36 -0300

    improve app

--- src/App.js
+++ src/App.js
@@ -4,24 +4,45 @@ const lastTokenUpdateDate = new Date(2023, 2, 7, 16, 21, 0)
 
 const App = () => {
   const diffDays = Math.floor((new Date() - lastTokenUpdateDate) / 1000 / 60 / (60 * 24))
-  const recordDays = 16
+  const lastRecordDay = 16
+  const currentRecordDays = lastRecordDay < diffDays ? diffDays : lastRecordDay
 
-  localStorage.setItem('LAST_DIFF_DAYS', diffDays)
-  localStorage.setItem('LAST_RECORD_DAYS', recordDays)
+  if (Number(localStorage.getItem('LAST_TOKEN_DATE')) !== lastTokenUpdateDate.getTime()) {
+    debugger
+    localStorage.setItem('LAST_TOKEN_DATE', lastTokenUpdateDate.getTime())
+  }
+
+  if (Number(localStorage.getItem('LAST_DIFF_DAYS')) !== diffDays) {
+    debugger
+    localStorage.setItem('LAST_DIFF_DAYS', diffDays)
+  }
+
+  // if (Number(localStorage.getItem('LAST_RECORD_DAYS')) < currentRecordDays) {
+  //   debugger
+  //   localStorage.setItem('LAST_RECORD_DAYS', currentRecordDays)
+  // }
+
+  const getReaction = () => {
+    if (!diffDays) return '😕'
+    if (diffDays <= currentRecordDays) return '🙂'
+    if (diffDays > currentRecordDays) return '😎'
+  }
+
 
   return (
     <div className="app">
       {/* <img src={calendar} alt="calendar" /> */}
       <header className='header'>
-        <p className='warn'>Por favor, pare de executar o compando <b>npm logout</b></p>
+        <p className='warn'>🛑 Por favor, pare de executar o compando <b>npm logout</b> 🛑</p>
       </header>
 
       <p>Estamos há</p>
       <p className='current-num-day'>{diffDays}</p>
+      <span>{getReaction()}</span>
       <p>dias sem <b><u>npm logout</u></b></p>
 
       <footer className='footer'>
-        <p>Nosso recorde é de <b className='record-num-day'>{recordDays}</b> dias 🥳</p>
+        <p>Nosso último recorde é de <b className='record-num-day'>{currentRecordDays}</b> dias 🥳</p>
       </footer>
     </div>
   );
