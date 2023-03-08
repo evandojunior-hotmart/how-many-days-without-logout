a1aaf35802d4795743edfb5ad1d7729fdb35d8db
Evando Junior <evando.junior@hotmart.com>
2023-03-08 00:15:26 -0300

    improve app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const lastTokenUpdateDate = new Date(2023, 2, 7, 16, 21, 0)
+
+const App = () => {
+  const diffDays = Math.floor((new Date() - lastTokenUpdateDate) / 1000 / 60 / (60 * 24))
+  const recordDays = 16
+
+  localStorage.setItem('LAST_DIFF_DAYS', diffDays)
+  localStorage.setItem('LAST_RECORD_DAYS', recordDays)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      {/* <img src={calendar} alt="calendar" /> */}
+      <header className='header'>
+        <p className='warn'>Por favor, pare de executar o compando <b>npm logout</b></p>
       </header>
+
+      <p>Estamos há</p>
+      <p className='current-num-day'>{diffDays}</p>
+      <p>dias sem <b><u>npm logout</u></b></p>
+
+      <footer className='footer'>
+        <p>Nosso recorde é de <b className='record-num-day'>{recordDays}</b> dias 🥳</p>
+      </footer>
     </div>
   );
 }
